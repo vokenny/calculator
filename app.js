9d1f9119b1440130b401e6076ab894db5bb58a17
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2021-10-16 13:21:15 +0100

    Feat(Working chained calculations with integers)

--- app.js
+++ app.js
@@ -1,39 +1,89 @@
 (function () {
   'use strict';
 
-  const screen = document.querySelector('#screen');
+  /* DOCUMENT SELECTORS */
+  const screenCalc = document.querySelector('#screen-calc');
+  const screenOperand = document.querySelector('#screen-operand');
   const clearButton = document.querySelector('#clear');
   const deleteButton = document.querySelector('#delete');
   const operandButtons = document.querySelectorAll('.operand');
+  const operatorButtons = document.querySelectorAll('.operator');
+  const equalButton = document.querySelector('#equal');
 
-  let onScreenValues = '';
+  let firstOperand = '';
+  let operator = '';
+  let currentOperand = '';
+
+  const getCurrentCalc = () => operator ? firstOperand + ' ' + operator : firstOperand;
 
   function addOperand(evt) {
-    if (onScreenValues.length < 15) {
-      onScreenValues += evt.target.value;
-      updateScreen();
+    if (currentOperand.length < 15) currentOperand += evt.target.value;
+  }
+
+  function addOperator(evt) {
+    if (!firstOperand) firstOperand = currentOperand;
+
+    currentOperand = '';
+    operator = evt.target.value;
+  }
+
+  function operate() {
+    if (firstOperand && currentOperand) {
+      switch (operator) {
+        case '+':
+          firstOperand = parseFloat(firstOperand) + parseFloat(currentOperand);
+          break;
+        case '-':
+          firstOperand = parseFloat(firstOperand) - parseFloat(currentOperand);
+          break;
+        case 'x':
+          firstOperand = parseFloat(firstOperand) * parseFloat(currentOperand);
+          break;
+        case '÷':
+          firstOperand = parseFloat(firstOperand) / parseFloat(currentOperand);
+          break;
+      }
+
+      clearCalc();
     }
   }
 
   function updateScreen() {
-    screen.textContent = onScreenValues;
+    screenCalc.textContent = getCurrentCalc();
+    screenOperand.textContent = currentOperand;
   }
 
-  function clearScreen() {
-    onScreenValues = '';
-    updateScreen();
+  function clearMemory() {
+    firstOperand = '';
+    operator = '';
+    currentOperand = '';
+  }
+
+  function clearCalc() {
+    operator = '';
+    currentOperand = '';
   }
 
   function deleteLastValue() {
-    onScreenValues = onScreenValues.slice(0, -1);
-    updateScreen();
+    currentOperand = currentOperand.slice(0, -1);
   }
 
   function addCalcEventListeners() {
-    clearButton.addEventListener('click', clearScreen);
-    deleteButton.addEventListener('click', deleteLastValue);
+    clearButton.addEventListener('click', () => { clearMemory(); updateScreen(); });
+    deleteButton.addEventListener('click', () => { deleteLastValue(); updateScreen(); });
+    equalButton.addEventListener('click', () => { operate(); updateScreen(); });
+
     operandButtons.forEach(operand =>
-      operand.addEventListener('click', (evt) => addOperand(evt)));
+      operand.addEventListener('click', (evt) => {
+        addOperand(evt);
+        updateScreen();
+      }));
+
+    operatorButtons.forEach(operator =>
+      operator.addEventListener('click', (evt) => {
+        addOperator(evt);
+        updateScreen();
+      }));
   }
 
   addCalcEventListeners();
