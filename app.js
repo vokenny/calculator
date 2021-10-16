fca86817ccbf5d8567efff3ca104782a62156b6a
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2021-10-16 12:22:36 +0100

    Feat(Clicking numbers, clear, or delete will update screen accordingly)

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
 (function () {
   'use strict';
 
+  const screen = document.querySelector('#screen');
+  const clearButton = document.querySelector('#clear');
+  const deleteButton = document.querySelector('#delete');
+  const operandButtons = document.querySelectorAll('.operand');
+
+  let onScreenValues = '';
+
+  function addOperand(evt) {
+    if (onScreenValues.length < 15) {
+      onScreenValues += evt.target.value;
+      updateScreen();
+    }
+  }
+
+  function updateScreen() {
+    screen.textContent = onScreenValues;
+  }
+
+  function clearScreen() {
+    onScreenValues = '';
+    updateScreen();
+  }
+
+  function deleteLastValue() {
+    onScreenValues = onScreenValues.slice(0, -1);
+    updateScreen();
+  }
+
+  function addCalcEventListeners() {
+    clearButton.addEventListener('click', clearScreen);
+    deleteButton.addEventListener('click', deleteLastValue);
+    operandButtons.forEach(operand =>
+      operand.addEventListener('click', (evt) => addOperand(evt)));
+  }
+
+  addCalcEventListeners();
 }())
